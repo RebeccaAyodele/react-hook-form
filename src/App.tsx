8374535f719881595c99c635fac79b493d791526
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-03-30 03:52:21 +0100

    Made the container implemented by useContext

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import Container from "./components/Container";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import TodoApp from "./components/TodoApp";
+import { useAppState } from "./components/AppStateContext";
 // import TaskCard from "./components/TaskCard";
 // import TaskForm from "./components/TaskForm";
 
 
 
 const App = () => {
+  const {state} = useAppState();
   const [isFormVisible, setIsFormVisible] = useState(false);
 
   const toggleForm = () => {
@@ -21,7 +23,10 @@ const App = () => {
       <div className="ml-[16rem] mt-[2rem]">
         <h1 className="text-2xl ml-[2rem] font-bold">Work Board</h1>
         <div className="flex justify-around">
-          <Container
+          {state.lists.map((list, i) => (
+            <Container types={list.text} description={list.description} heading={list.heading} />
+          ))}
+          {/* <Container
             types="To-Do"
             heading="No task yet"
             description="Add a new task to get started"
@@ -33,7 +38,7 @@ const App = () => {
           <Container
             types="Completed"
             description="Task that are completed will appear here"
-          />
+          /> */}
           <TodoApp isFormVisible={isFormVisible} setIsFormVisible={setIsFormVisible} />
         </div>
         {/* <TaskForm /> */}
