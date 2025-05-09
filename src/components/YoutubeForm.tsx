adda88d57751699eb97aa1693cc35a7de884df46
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-09 19:10:16 +0100

    Default values

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -10,7 +10,18 @@ type FormValues = {
 };
 
 const YoutubeForm = () => {
-  const form = useForm<FormValues>();
+  const form = useForm<FormValues>({
+    defaultValues: async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users/1");
+      const data = await response.json()
+      return {
+        username: "Batman",
+        email: data.email,
+        channel: ""
+      }
+    }
+  }
+  );
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
 
