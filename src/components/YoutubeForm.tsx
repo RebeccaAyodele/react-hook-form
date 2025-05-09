6e3e6588df48aa93a6228d99bd230c44d063d009
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-09 17:32:11 +0100

    Form submission

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,20 +1,42 @@
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+let renderCount = 0
+
+type FormValues = {
+  username: string,
+  email: string,
+  channel: string,
+}
+
 const YoutubeForm = () => {
-    return (
-      <div>
-        <form>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" />
-  
-          <label htmlFor="email">E-mail</label>
-          <input type="email" id="email" name="email" />
-  
-          <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" name="channel" />
-  
-          <button>Submit</button>
-        </form>
-      </div>
-    )
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  const onSubmit = (data: FormValues) => {
+    console.log("Form Submitted", data);
+    
   }
-  
-  export default YoutubeForm
+
+  renderCount++
+  return (
+    <div>
+      <h1>YouTube Form ({renderCount / 2})</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" {...register("username")} />
+
+        <label htmlFor="email">E-mail</label>
+        <input type="email" id="email" {...register("email")} />
+
+        <label htmlFor="channel">Channel</label>
+        <input type="text" id="channel" {...register("channel")} />
+
+        <button>Submit</button>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
+};
+
+export default YoutubeForm;
