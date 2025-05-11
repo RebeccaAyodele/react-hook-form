ff2312dcfcc5ce3aa89ed18a6f7530ff7c714305
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-05-11 19:02:06 +0100

    Zod integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css"
+import ZodYouTubeForm from "./components/ZodYoutubeForm"
 // import YoutubeForm from "./components/YoutubeForm"
-import YupYouTubeForm from "./components/YupYouTubeForm"
+// import YupYouTubeForm from "./components/YupYouTubeForm"
 
 const App = () => {
   return (
     <div>
       {/* <YoutubeForm /> */}
-      <YupYouTubeForm />
+      {/* <YupYouTubeForm /> */}
+      <ZodYouTubeForm />
     </div>
   )
 }
